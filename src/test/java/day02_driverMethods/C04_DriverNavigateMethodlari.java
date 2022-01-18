95b2007c5d2c37f01734c465036ff18fdff70fdb
mert dogan <trsverige466@gmail.com>
2022-01-18 21:04:10 +0300

    day2

---
+++ src/test/java/day02_driverMethods/C04_DriverNavigateMethodlari.java
@@ -0,0 +1,24 @@
+package day02_driverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C04_DriverNavigateMethodlari {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","resources/drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+
+        driver.navigate().to("https://www.amazon.com");
+        // DRİVER.GET İLE AYNİ İSLEMİ YAPAR AMA DAHA HİZLİDİR
+
+        driver.navigate().to("https://www.techproeducation.com");
+
+        driver.navigate().back();
+
+        driver.navigate().forward();
+
+        driver.navigate().refresh();
+
+    }
+}
